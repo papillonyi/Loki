ecaf5f8c05ba999d4607a4400e7b5742a8d472b7
Andy <cy39@live.cn>
2019-02-26 19:12:08 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import {view as CitySelector} from './input_area/';
+import {view as Weather} from './detail_area/';
+import {view as Weather} from './total_asset/';
+
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+            <CitySelector />
+            <Weather />
+            </div>
+    );
+    }
+}
+
+export default App;
