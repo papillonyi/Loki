a418182ad20ad2640d0eb56e4f70dc072061bd08
Andy <cy39@live.cn>
2019-02-26 21:01:00 +0800

    try

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
-import {view as CitySelector} from './input_area/';
-import {view as Weather} from './detail_area/';
+// import {view as CitySelector} from './input_area/';
+// import {view as Weather} from './detail_area/';
 import {view as Weather} from './total_asset/';
 
 
--- src/store.js
+++ src/store.js
@@ -2,18 +2,15 @@ import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
 
 import thunkMiddleware from 'redux-thunk'
 
-import {reducer as weatherReducer} from './weather/';
+import {reducer as totalAssetReducer} from './total_asset/';
 
 
 
 const reducer = combineReducers({
-    weather: weatherReducer
+    totalAsset: totalAssetReducer
 });
 
 const middlewares = [thunkMiddleware];
-if (process.env.NODE_ENV !== 'production') {
-    middlewares.push(require('redux-immutable-state-invariant')());
-}
 
 const storeEnhancers = compose(
     applyMiddleware(...middlewares),
